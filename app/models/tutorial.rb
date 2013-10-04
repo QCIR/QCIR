3b9f9051f013f486d24309a2828fb8222bb2e435
= <=>
2013-10-04 14:11:46 -0300

    conserto de tutorial e dados no index

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,12 +1,10 @@
 class Tutorial < ActiveRecord::Base
 
 	#validates
-	  validates_presence_of :title, :description, :user_id, :tag_ids
+	  validates_presence_of :title, :description, :user_id
 
 	#relations
 	  belongs_to :user
-	  has_many :comments
 	  has_many :stages
-	  has_and_belongs_to_many :tags
 
 end
